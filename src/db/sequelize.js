6bcc37d1f15bbe8927b35c5fbcd7ffec4a678df7
ng1n3 <code.with.muyiwa@gmail.com>
2024-11-01 17:28:45 +0100

    temp_fix: added extra detail to sequelizejs

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || 'localhost',
     dialect: "postgres",
     port: 5432,
     logging: console.log,
